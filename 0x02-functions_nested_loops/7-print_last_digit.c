b465103c5ee16a39e41e646ecc2f0833cedf6936
hareg <haregalemu9@gmail.com>
2022-12-13 12:54:44 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,17 @@
 /**
  * main - check the code
  * Return: Always 0.
- */   
+ */
 
 int main(void)
 {
 	    int r;
 
-	        print_last_digit(98);
+    	    print_last_digit(98);
 		print_last_digit(0);
 		r = print_last_digit(-10);
 		_putchar('0' + r);
 		_putchar('\n');
 
 			return (0);
-
 }
