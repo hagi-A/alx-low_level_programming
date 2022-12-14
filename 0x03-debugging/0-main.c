cd7bf49e44d856ac7b52fbbf3eba516f81ba0d1e
hareg <haregalemu9@gmail.com>
2022-12-14 12:39:12 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tasts function tha prints that if an integer is positive or negative
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
