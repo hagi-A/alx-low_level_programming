673a478bd597be51ea398df18555ba00a61b7def
hareg <haregalemu9@gmail.com>
2022-12-21 11:23:38 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string to another string
+ * @dest: the buffer
+ * @src: the string to be copied
+ * @n: the number of bytes to include in @dest
+ * Return: pointer to the resulting string @dest (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
