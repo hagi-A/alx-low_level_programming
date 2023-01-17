10994fb1319c3fc88da892292942fec5fba6f5eb
hareg <haregalemu9@gmail.com>
2023-01-17 10:26:48 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *name;
 	int age;
 	char *owner;
-} dog;
+};
 
 typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -21,4 +21,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _putchar(char c);
+
 #endif
