1ac0b3756060fea8d425f01fb947502c52f84212
Hareg <haregalemu9@gmail.com>
2023-01-31 11:02:32 -0800

    task 0 and 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- *
- *  *               in a linked listint_t list.
+ * listint_len - Returns the number of elements in a linked listint_t list.
  *
  *   * @h: A pointer to the head of the listint_t list.
  *
