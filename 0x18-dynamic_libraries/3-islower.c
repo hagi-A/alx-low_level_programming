16b8a55ce74c1776070bb3c2a3b7adb7ddb3eaa0
Hareg <haregalemu9@gmail.com>
2023-03-20 12:34:23 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Single description, single line
+ * @c: contains value to be compared
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
